240379bfeb28d4fb30ba77e784ddf1a7bcad50aa
maksik08 <maximpometco@mail.ru>
2026-05-27 15:15:24 +0300

    refactor(reviews): drop client-side feedback store and seed mocks

--- src/entities/product-feedback/index.ts
+++ src/entities/product-feedback/index.ts
@@ -1,5 +1,3 @@
-export { getProductRatingSummary, useProductFeedbackStore } from './model/feedback.store'
-export type { ProductReview } from './model/feedback.store'
 export { StarRating } from './ui/StarRating'
 export {
   fetchProductReviews,
@@ -9,4 +7,5 @@ export {
   useCreateReviewMutation,
   useDeleteReviewMutation,
   productReviewKeys,
+  getRatingSummary,
 } from './api/reviews.api'
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+if (typeof window !== 'undefined') {
+  window.localStorage.removeItem('product-feedback')
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
